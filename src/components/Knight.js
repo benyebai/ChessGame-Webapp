e6a89fa345e9be662cb144dd859bdb64cfa82949
trisexual <mrthomaszheng@gmail.com>
2021-08-10 19:44:44 -0700

    updated shit

--- src/components/Knight.js
+++ src/components/Knight.js
@@ -6,16 +6,19 @@ export function Knight({index, team}){
 
     let imgRef = team + "Knight.png";
 
-    const [collected, drag, preview] = useDrag(() => ({
+    const [{isDragging}, drag, preview] = useDrag(() => ({
         type : "chessPiece",
-        item: { pos:index }
+        item: { pos:index },
+        collect: (monitor) => ({
+            isDragging : !!monitor.isDragging()
+        })
       }))
     
     return(
         <>
         <DragPreviewImage connect={preview} src = {`/images/${imgRef}`} />
         <div ref = {drag}>
-            <img src = {`/images/${imgRef}`} className = "chessPiece" style = {{width:"100px", opacity:1}}/>
+            <img src = {`/images/${imgRef}`} className = "chessPiece" style = {{width:"100px", opacity: isDragging ? 0 : 1}}/>
         </div>
         </>
     );
